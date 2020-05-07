9914045f9856eadbf49126b0daf26380acfb01a9
NickyRuby <nikita.lobyntsev@gmail.com>
2020-05-07 07:50:47 +0300

    Make all timesheets endpoints

--- api/timesheets.js
+++ api/timesheets.js
@@ -3,6 +3,18 @@ const timeSheetsRouter = new express.Router({mergeParams: true});
 const sqlite = require('sqlite3');
 const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+timeSheetsRouter.param('timesheetId', (req,res,next,timesheetId) => {
+    db.get(`SELECT * FROM Timesheet WHERE id=${timesheetId}`, (err,data) => {
+        if (!data) {
+            res.status(404).send()
+        }
+        else {
+            req.timesheetId = timesheetId;
+            next();
+        }
+    })
+})
+
 timeSheetsRouter.get('/', (req,res,next) => {
     db.all(`SELECT * FROM Timesheet WHERE employee_id=${req.employeeId};`,(err, data) => {
         if (err) {
@@ -11,6 +23,20 @@ timeSheetsRouter.get('/', (req,res,next) => {
         res.status(200).json({timesheets: data});
     })
 });
+
+
+const validateTimesheet = (req,res,next) => {
+    req.hours = req.body.timesheet.hours;
+    req.rate = req.body.timesheet.rate;
+    req.date = req.body.timesheet.date;
+
+    if (!req.hours || !req.rate || !req.date) {
+        res.status(400).send();
+    }
+    else {
+        next();
+    }
+}
 
 
 timeSheetsRouter.post('/', (req,res,next) =>{
@@ -30,6 +56,33 @@ timeSheetsRouter.post('/', (req,res,next) =>{
          }
     })
 });
+
+
+timeSheetsRouter.put('/:timesheetId', validateTimesheet, (req,res,next) => {
+    db.run(`UPDATE Timesheet SET 
+    hours=${req.hours}, 
+    rate=${req.rate}, 
+    date=${req.date}, 
+    employee_id=${req.employeeId} 
+    WHERE id=${req.params.timesheetId};`, function(err) {
+        if (err) {
+            next(err);
+        }
+        db.get(`SELECT * FROM Timesheet WHERE id=${req.params.timesheetId}`, (err, data) => {
+            res.status(200).json({timesheet: data});
+        });
+    })
+});
+
+timeSheetsRouter.delete('/:timesheetId', (req,res,next) => {
+    db.run(`DELETE FROM Timesheet WHERE id=${req.params.timesheetId};`, function(err) {
+        if (err) {
+            next(err);
+        }
+        res.status(204).send();
+    })
+});
+
 
 
 module.exports = timeSheetsRouter;
